8516e18dbc98e13b1252df8efc46b66f82abdc3f
dmcin003e <dmcin003@fiu.edu>
2025-09-18 17:43:15 -0400

    finished tutorial 4

---
+++ tutorial4.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
+print(air_quality.head())
+air_quality.plot()
+plt.show()
+air_quality['station_paris'].plot()
+plt.show()
+air_quality.plot.scatter(x='station_london', y='station_paris', alpha=0.5)
+plt.show()
+air_quality.plot.box()
+plt.show()
+axs = air_quality.plot.area(figsize=(12,4), subplots=True)
+plt.show()
+fig, axs = plt.subplots(figsize=(12,4))
+air_quality.plot.area(ax=axs)
+axs.set_ylabel('NO$_2$ concentration')
+fig.savefig('no2_concentration.png')
+plt.show()
